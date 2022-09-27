6a9dbf7085f9cb6273d2b1b678f775198d15d2f8
Anteneh Solomon <antenehcs@gmail.com>
2022-09-27 16:58:10 +0300

    initial commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, HashRouter, Route,Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar/navbar';
+import Home from './Components/Body/Home/Home';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold underline text-red-200'>Hello blacknebecom</h1>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
