6cf4e598df368dba5b17fbb67d60da60ac3c50b8
Anteneh Solomon <antenehcs@gmail.com>
2022-09-30 13:30:27 +0300

    home

--- src/Components/Body/Home/Home.tsx
+++ src/Components/Body/Home/Home.tsx
@@ -1,29 +1,37 @@
-import React from 'react'
-import Carditems from '../../Cards/Carditems'
+import React,{useState} from 'react';
+import Carditems from '../../Cards/Carditems';
+import Menu from '../../../Icons/menu.png';
+import close from '../../../Icons/close.png';
 
 const Home = () => {
+  const [open, setOpen]= useState(true);
   return (
     <div className=''>
         <div className='shadow border-b-2 border-gray-400 mx-4 md:mx-32 mt-8'>
           <h1>Shop every item You want</h1>
         </div>
         <div className='flex flex-row'>
-          <div className='h-96 w-72 shadow m-4 rounded-md border-2'>
-            <h2 className='border-b-2 border-gray-400 mx-14'>Filter</h2>
-            <div className='flex justify-start'>
+          <div className='flex flex-col'>
+            <div onClick={()=>setOpen(!open)} className='mx-2 w-6 mb-2'>
+              {open? <img  className='h-4 mt-2'  src={close}/> : <img className='h-4 mt-2' src={Menu}/>}
+            </div>
+            <div className={`${open? "block" : "hidden"} h-96 w-32 shadow mx-2 rounded-md border-2`}>
+              <h2 className='border-b-2 border-gray-400'>Filter</h2>
+            <div className={`flex justify-start`}>
               <ul className='flex flex-col m-4'>
-                <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>PC</button></li>
-                <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Laptops</button></li>
-                <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Phones</button></li>
-                <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Joysticks</button></li>
-                <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Tv</button></li>
-                <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Charger</button></li>
-                <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>USB Cables</button></li>
-                <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>PlayStations</button></li>
-                <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Power Bank</button></li>
-                <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Hard Disk</button></li>
+                <li className='py-1 hover:border-l-2 border-black text-left'><button>PC</button></li>
+                <li className='py-1 hover:border-l-2 border-black text-left'><button>Laptops</button></li>
+                <li className='py-1 hover:border-l-2 border-black text-left'><button>Phones</button></li>
+                <li className='py-1 hover:border-l-2 border-black text-left'><button>Joysticks</button></li>
+                <li className='py-1 hover:border-l-2 border-black text-left'><button>Tv</button></li>
+                <li className='py-1 hover:border-l-2 border-black text-left'><button>Charger</button></li>
+                <li className='py-1 hover:border-l-2 border-black text-left'><button>USB Cables</button></li>
+                <li className='py-1 hover:border-l-2 border-black text-left'><button>PlayStations</button></li>
+                <li className='py-1 hover:border-l-2 border-black text-left'><button>Power Bank</button></li>
+                <li className='py-1 hover:border-l-2 border-black text-left'><button>Hard Disk</button></li>
               </ul>
             </div>
+          </div>
           </div>
           <div className='flex justify-around flex-wrap overflow-y-scroll h-[28rem] pr-8'>
             <Carditems/>
@@ -51,10 +59,10 @@ const Home = () => {
         <div className='shadow border-b-2 border-gray-400 mx-4 md:mx-32 mt-8'>
           <h1>Shop By Class</h1>
         </div>
-        <div className='flex flex-row flex-wrap justify-around p-4 mt-4'>
+        <div className='grid grid-cols-1 md:grid-cols-2 justify-around p-4 mt-4'>
           <div>
-            <h2 className='border-b-2 border-gray-400 mx-14'>Trending</h2>
-            <div className='overflow-x-scroll w-72'>
+            <h2 className='border-b-2 border-gray-400 mx-32 md:mx-80'>Trending</h2>
+            <div className='overflow-x-scroll mx-20 my-8 h-48 md:h-72'>
               <div className='flex flex-row  p-4'>
                 <Carditems/>
                 <Carditems/>
@@ -66,10 +74,9 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className='md:border-r-2 border-black h-72 border-gray-400'></div>
           <div>
-          <h2 className='border-b-2 border-gray-400 mx-14'>New Arrivals</h2>
-          <div className='overflow-x-scroll w-72'>
+          <h2 className='border-b-2 border-gray-400 mx-32 md:mx-80'>New Arrivals</h2>
+          <div className='overflow-x-scroll mx-20 my-8 h-48 md:h-72'>
             <div className='flex flex-row  p-4 '>
                   <Carditems/>
                   <Carditems/>
@@ -79,10 +86,9 @@ const Home = () => {
                 </div>
             </div>
           </div>
-          <div className='border-r-2 border-black h-72 border-gray-400'></div>
           <div>
-            <h2 className='border-b-2 border-gray-400 mx-14'>Discounts</h2>
-            <div className='overflow-x-scroll w-72'>
+            <h2 className='border-b-2 border-gray-400 mx-32 md:mx-80'>Discounts</h2>
+            <div className='overflow-x-scroll mx-20 my-8 h-48 md:h-72'>
               <div className='flex flex-row  p-4 '>
                 <Carditems/>
                 <Carditems/>
@@ -92,10 +98,9 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className='border-r-2 border-black h-72 border-gray-400'></div>
           <div>
-            <h2 className='border-b-2 border-gray-400 mx-14'>Popular Brands</h2>
-            <div className='overflow-x-scroll w-72'>
+            <h2 className='border-b-2 border-gray-400 mx-28 md:mx-72'>Popular Brands</h2>
+            <div className='overflow-x-scroll mx-20 my-8 h-48 md:h-72'>
               <div className='flex flex-row  p-4 '>
                 <Carditems/>
                 <Carditems/>
--- src/Components/Cards/Carditems.tsx
+++ src/Components/Cards/Carditems.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Carditems = () => {
   return (
-    <div className='h-48 shadow w-48 rounded-md m-2 p-2'>
-      <div className='h-36 w-44'>
+    <div className='h-32 md:h-48 md:shadow md:w-48 rounded-md m-2 p-2'>
+      <div className='h-32 w-28 md:h-36 md:w-44'>
         <img src='' alt=''/>
       </div>
       <div>
