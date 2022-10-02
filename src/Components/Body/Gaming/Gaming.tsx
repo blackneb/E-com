7289cbcf737fa4f9dc92dc555b11055a994d7a06
Anteneh Solomon <antenehcs@gmail.com>
2022-10-02 13:35:43 +0300

    gaming pagination

--- src/Components/Body/Gaming/Gaming.tsx
+++ src/Components/Body/Gaming/Gaming.tsx
@@ -8,6 +8,12 @@ import Stack from '@mui/material/Stack';
 
 const Gaming = () => {
   const [open, setOpen]= useState(false);
+  const [page, SetPage] = useState(1);
+
+  const handleChange = (e:any, p:any) => {
+    console.log(p);
+    SetPage(p);
+  }
   return (
     <div>
           <div className='flex flex-row'>
@@ -54,7 +60,7 @@ const Gaming = () => {
         </div>
         <div className='flex justify-center my-8'>
           <Stack spacing={2}>
-            <Pagination count={10} variant="outlined" shape="rounded" />
+            <Pagination count={10} variant="outlined" shape="rounded" onChange={handleChange} />
           </Stack>
         </div>
     </div>
