651c575f8c26dbc74dcd0564e62c48f96666236d
Anteneh Solomon <antenehcs@gmail.com>
2022-10-02 20:51:33 +0300

    carditems

--- src/Components/Cards/Carditems.tsx
+++ src/Components/Cards/Carditems.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import iphone from '../../Images/iphone.jpg'
+import {Link} from 'react-router-dom'
 
 interface propsitem {
   afor: string
@@ -17,9 +18,11 @@ const Carditems = (props: propsitem) => {
   console.log(name);
   return (
     <div className='h-52 hover:border-2 hover:border-gray-400 shadow md:h-48 md:shadow md:w-48 rounded-md m-2 p-2'>
-      <div className='h-32 w-28 flex justify-center md:h-36 md:w-44'>
-        <img className='h-32 md:mt-0 rounded-md' src={"https://blackneb.com/piyankiya/api/post/photos/" + photos} alt=''/>
-      </div>
+      <Link  to={`/detailed/${props.id}`}>
+        <div className='h-32 w-28 flex justify-center md:h-36 md:w-44'>
+          <img className='h-32 md:mt-0 rounded-md' src={"https://blackneb.com/piyankiya/api/post/photos/" + photos} alt=''/>
+        </div>
+      </Link>
       <div>
         <button className='border-2 border-black rounded-2xl bg-white text-black px-2 hover:bg-black hover:text-white m-4 md:m-0'>Cart Item</button>
       </div>
