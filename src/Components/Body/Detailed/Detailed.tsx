63a845815baed4bfb667ce7080e8ef0ad2e849d3
Anteneh Solomon <antenehcs@gmail.com>
2022-10-02 20:46:09 +0300

    detailed

--- src/Components/Body/Detailed/Detailed.tsx
+++ src/Components/Body/Detailed/Detailed.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import iphone from '../../../Images/iphone.jpg'
 
 const Detailed = () => {
   return (
     <div>
-      <h1>This is detailed page</h1>
+      <div className='shadow border-b-2 border-gray-400 mx-4 md:mx-16 mt-8'>
+          <h1>View Detail</h1>
+        </div>
+      <div className='flex justify-center items-center pt-8'>
+        <div className='shadow rounded-md'>
+          <img className='h-96 rounded-md' src={iphone} alt=''/>
+        </div>
+      </div>
     </div>
   )
 }
