e6bec4f350e22a02500e56311ee7125f1589043c
Anteneh Solomon <antenehcs@gmail.com>
2022-10-03 19:29:50 +0300

    redux

---
+++ src/Models/Models.ts
@@ -0,0 +1,18 @@
+export interface Ilaptop{
+
+}
+export interface Iphone{
+
+}
+export interface Itv{
+
+}
+export interface Iaccessories{
+
+}
+export interface Ibook{
+
+}
+export interface Iimages{
+
+}
---
+++ src/Redux/ActionTypes.ts
@@ -0,0 +1,6 @@
+export const ADD_LAPTOP = 'ADD_LAPTOP';
+export const ADD_PHONE = 'ADD_PHONE';
+export const ADD_TV = 'ADD_TV';
+export const ADD_ACCESSORIES = 'ADD_ACCESSORIES';
+export const ADD_BOOK = 'ADD_BOOKS';
+export const ADD_IMAGES = 'ADD_IMAGES';
