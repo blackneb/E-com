cb722ea7e480d26054b0b0022a8677e978c13bc5
Anteneh Solomon <antenehcs@gmail.com>
2022-10-05 10:05:15 +0300

    management component

--- src/Components/Body/Management/Management.tsx
+++ src/Components/Body/Management/Management.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Management = () => {
+    const [currentcomponent, setcurrentcomponent] = useState<string>()
+    const components:{name:string,component:string}[] = [
+        {name:"Laptops",component:""},
+        {name:"Phones",component:""},
+        {name:"Tv",component:""},
+        {name:"Accessories",component:""},
+    ]
   return (
     <div>
-        <div className=''>
+        <div className='mx-96 mt-4'>
+            <ul className='flex flex-row justify-between'>
+                {
+                    components.map((items) => (
+                        <li className='hover:border-b-4'>
+                            <p onClick={()=> setcurrentcomponent(items.component)}>{items.name}</p>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     </div>
   )
