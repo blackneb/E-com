e25cc1095015dd082622bfe1008753784334755f
Anteneh Solomon <antenehcs@gmail.com>
2022-10-22 11:33:37 +0300

    laptops

--- src/Components/Body/Laptops/Laptops.tsx
+++ src/Components/Body/Laptops/Laptops.tsx
@@ -1,32 +1,49 @@
 import React,{useState, useEffect} from 'react'
 import Menu from '../../../Icons/menu.png';
+import { Carditems } from '../../Cards/Carditems';
 import close from '../../../Icons/close.png';
-import Carditems from '../../Cards/Carditems';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import axios from 'axios';
 import { Card } from '@mui/material';
 
-interface propsitem {
-  afor: string
-  description: string
-  gfor: string
-  id: string
-  name: string
-  photos: string
-  price: string
-  types: string
+interface laptops{
+  id:string;
+  brand:string;
+  name:string;
+  ram:string;
+  catagory:string;
+  display:string;
+  storagessd:string;
+  storagehdd:string;
+  cpuprocessor:string;
+  cpugeneration:string;
+  graphicscardname:string;
+  graphicscardsize:string;
+  battery:string;
+  screensize:string;
+  price:string;
+  description:string;
+  types:string;
+  isdeleted:string;
+  images:Array<string>
 }
 
 const Laptops = () => {
   const [open, setOpen] = useState(false);
   const [page, SetPage] = useState(1);
+  const [itemsthere, setitemsthere] = useState(false);
   const [post,setpost] = useState<any[]>([]);
 
   useEffect(()=>{
-    axios.get("http://blackneb.com/piyankiya/api/post/read.php").then((response) => {
-      setpost(response.data.data);
+    axios.get("http://localhost/blacknebecom/api/post/read_laptop.php").then((response) => {
       console.log(response.data.data);
+      if(response.data.data === "no posts found"){
+        setitemsthere(true);
+      }
+      else{
+        setpost(response.data.data);
+      }
     });
   },[]);
   if(!post) return null;
@@ -36,34 +53,38 @@ const Laptops = () => {
   }
   return (
     <div>
-          <div className='flex flex-row'>
-            <div className='flex flex-col'>
-              <div onClick={()=>setOpen(!open)} className='md:hidden mx-2 w-6 mb-2'>
-                {open? <img  className='h-4 mt-2'  src={close}/> : <img className='h-4 mt-2' src={Menu}/>}
-              </div>
-              <div className={`${open? "block" : "hidden"} md:block md:mt-4 w-36 shadow mx-2 rounded-md`}>
-                <h2 className='border-b-2 border-gray-400 mx-12'>Filter</h2>
-              <div className={`flex justify-start overflow-x-scroll h-72`}>
-                <ul className='flex flex-col m-4 '>
-                  <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Brand</button></li>
-                  <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>RAM</button></li>
-                  <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Graphics Card</button></li>
-                  <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Generation</button></li>
-                  <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Price</button></li>
-                  <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Storage</button></li>
-                  <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Disaplay</button></li>
-                </ul>
-              </div>
-            </div>
+          <div className='shadow border-b-2 border-gray-400 mx-4 md:mx-16 mt-8'>
+            <h1>Shop Brand Laptops</h1>
           </div>
-          <div className='flex justify-around flex-wrap pr-8 mt-2'>
-            {
-              [...post].slice((page-1)*6,6*page).map(({afor, description, gfor, id, name, photos, price, types}:propsitem) => (
-                <Carditems key={id} afor={afor} description={description} gfor={gfor} id={id} name={name} photos={photos} price={price} types={types}/>
-                ))
-            }
-          </div>
-        </div>
+          {(()=>{
+              if(itemsthere){
+                  return(
+                    <div className='mt-10'>
+                       <h1>we will add contents soon</h1>
+                    </div>
+                    )
+              }
+              else{
+                  return(
+                    <div className='flex flex-row'>
+                      <div className='flex flex-col'>
+                        <div onClick={()=>setOpen(!open)} className='md:hidden mx-2 w-6 mb-2'>
+                          {open? <img  className='h-4 mt-2'  src={close}/> : <img className='h-4 mt-2' src={Menu}/>}
+                        </div>
+                      </div>
+                    <div className='flex flex-row'>
+                      <div className='mx-16 flex justify-around flex-wrap pr-8 mt-2'>
+                        {
+                          [...post].slice((page-1)*6,6*page).map(({id,brand,name,catagory,price,description,types,images}:laptops) => (
+                            <Carditems key={id} id={id} brand={brand} catagory={catagory} name={name} price={price} description={description} types={types} images={images}/>
+                            ))
+                        }
+                      </div>
+                    </div>
+                  </div>
+                  )
+              }
+          })()}
         <div className='flex justify-center my-8'>
           <Stack spacing={2}>
             <Pagination count={10} variant="outlined" shape="rounded" onChange={handleChange} />
