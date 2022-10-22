9748259cc000452edbed5aa742060c8d1fed3667
Anteneh Solomon <antenehcs@gmail.com>
2022-10-22 11:34:19 +0300

    phone

--- src/Components/Body/Phone/Phone.tsx
+++ src/Components/Body/Phone/Phone.tsx
@@ -1,40 +1,83 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import Menu from '../../../Icons/menu.png';
+import { Carditems } from '../../Cards/Carditems';
+import axios from 'axios';
 import close from '../../../Icons/close.png';
-import Carditems from '../../Cards/Carditems';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
+interface phone {
+  id:string;
+  brand:string;
+  name:string;
+  catagory:string;
+  ram:string;
+  storage:string;
+  color:string;
+  screensize:string;
+  camerafront:string;
+  cameraback:string;
+  price:string;
+  description:string;
+  types:string;
+  isdeleted:string;
+  images:Array<string>;
+}
 const Phone = () => {
   const [open, setOpen]= useState(false);
   const [page, SetPage] = useState(1);
+  const [itemsthere, setitemsthere] = useState(false);
+  const [post,setpost] = useState<any[]>([]);
 
   const handleChange = (e:any, p:any) => {
     console.log(p);
     SetPage(p);
   }
+  useEffect(()=>{
+    axios.get("http://localhost/blacknebecom/api/post/read_phone.php").then((response) => {
+      console.log(response.data.data);
+      if(response.data.data === "no posts found"){
+        setitemsthere(true);
+      }
+      else{
+        setpost(response.data.data);
+      }
+    });
+  },[]);
+  if(!post) return null;
   return (
     <div>
-          <div className='flex flex-row'>
-            <div className='flex flex-col'>
-              <div onClick={()=>setOpen(!open)} className='mx-2 md:hidden w-6 mb-2'>
-                {open? <img  className='h-4 mt-2'  src={close}/> : <img className='h-4 mt-2' src={Menu}/>}
-              </div>
-              <div className={`${open? "block" : "hidden"} md:block mt-4 w-36 shadow mx-2 rounded-md`}>
-                <h2 className='border-b-2 border-gray-400 mx-12'>Filter</h2>
-              <div className={`flex justify-start overflow-x-scroll`}>
-                <ul className='flex flex-col m-4 '>
-                  <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Brand</button></li>
-                  <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>RAM</button></li>
-                  <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Price</button></li>
-                  <li className='py-1 hover:border-l-2 border-black text-left p-1'><button>Storage</button></li>
-                </ul>
-              </div>
-            </div>
+          <div className='shadow border-b-2 border-gray-400 mx-4 md:mx-16 mt-8'>
+            <h1>Shop Brand New Phones</h1>
           </div>
-          <div className='flex justify-around flex-wrap pr-8 mt-2'>
-            
-          </div>
-        </div>
+          {(()=>{
+              if(itemsthere){
+                  return(
+                    <div className='mt-10'>
+                       <h1>we will add contents soon</h1>
+                    </div>
+                    )
+              }
+              else{
+                  return(
+                    <div className='flex flex-row'>
+                      <div className='flex flex-col'>
+                        <div onClick={()=>setOpen(!open)} className='md:hidden mx-2 w-6 mb-2'>
+                          {open? <img  className='h-4 mt-2'  src={close}/> : <img className='h-4 mt-2' src={Menu}/>}
+                        </div>
+                      </div>
+                    <div className='flex flex-row'>
+                      <div className='mx-16 flex justify-around flex-wrap pr-8 mt-2'>
+                        {
+                          [...post].slice((page-1)*6,6*page).map(({id,brand,name,catagory,price,description,types,images}:phone) => (
+                            <Carditems key={id} id={id} brand={brand} catagory={catagory} name={name} price={price} description={description} types={types} images={images}/>
+                            ))
+                        }
+                      </div>
+                    </div>
+                  </div>
+                  )
+              }
+          })()}
         <div className='flex justify-center my-8'>
           <Stack spacing={2}>
             <Pagination count={10} variant="outlined" shape="rounded" onChange={handleChange} />
