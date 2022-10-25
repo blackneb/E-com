9db4245ddd94de9a62420766272515ff89453a00
Anteneh Solomon <antenehcs@gmail.com>
2022-10-25 21:59:05 +0300

    action types

--- src/Redux/ActionTypes.ts
+++ src/Redux/ActionTypes.ts
@@ -4,4 +4,5 @@ export const ADD_TV = 'ADD_TV';
 export const ADD_ACCESSORIES = 'ADD_ACCESSORIES';
 export const ADD_BOOK = 'ADD_BOOKS';
 export const ADD_IMAGES = 'ADD_IMAGES';
+export const ADD_ALL = 'ADD_ALL';
 export const ADD_USER_AUTORIZATION = 'ADD_USER_AUTORIZATION';
