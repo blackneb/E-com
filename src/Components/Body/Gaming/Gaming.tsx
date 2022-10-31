0769eafde226e6a6df7a3fc7b431d60a53c09a26
Anteneh Solomon <antenehcs@gmail.com>
2022-10-31 11:19:36 +0300

    gaming

--- src/Components/Body/Gaming/Gaming.tsx
+++ src/Components/Body/Gaming/Gaming.tsx
@@ -1,33 +1,56 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import Menu from '../../../Icons/menu.png';
+import { Carditems } from '../../Cards/Carditems';
 import close from '../../../Icons/close.png';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { Card } from '@mui/material';
 
-
-const Gaming = () => {
-  const [open, setOpen]= useState(false);
+const Laptops = () => {
+  const [open, setOpen] = useState(false);
   const [page, SetPage] = useState(1);
-
+  const [itemsthere, setitemsthere] = useState(false);
+  const gaming = useSelector((state:any) => state.allitems.allitems);
+  let gameitems = gaming.filter((game:any) => game.types === 'gaming');
   const handleChange = (e:any, p:any) => {
-    console.log(p);
     SetPage(p);
   }
   return (
     <div>
           <div className='shadow border-b-2 border-gray-400 mx-4 md:mx-16 mt-8'>
-            <h1>Shop Gaming Devices</h1>
+            <h1>Shop Brand Gaming Consoles</h1>
           </div>
-          <div className='flex flex-row'>
-            <div className='flex flex-col'>
-              <div onClick={()=>setOpen(!open)} className='mx-2 md:hidden w-6 mb-2'>
-                {open? <img  className='h-4 mt-2'  src={close}/> : <img className='h-4 mt-2' src={Menu}/>}
-              </div>
-          </div>
-          <div className='flex justify-around flex-wrap pr-8 mt-2'>
-            
-          </div>
-        </div>
+          {(()=>{
+              if(itemsthere){
+                  return(
+                    <div className='mt-10'>
+                       <h1>we will add contents soon</h1>
+                    </div>
+                    )
+              }
+              else{
+                  return(
+                    <div className='flex flex-row'>
+                      <div className='flex flex-col'>
+                        <div onClick={()=>setOpen(!open)} className='md:hidden mx-2 w-6 mb-2'>
+                          {open? <img  className='h-4 mt-2'  src={close}/> : <img className='h-4 mt-2' src={Menu}/>}
+                        </div>
+                      </div>
+                    <div className='flex flex-row h-[32rem]'>
+                      <div className='mx-16 flex justify-around flex-wrap pr-8 mt-2'>
+                        {
+                          [...gameitems].slice((page-1)*12,12*page).map(({id,brand,name,catagory,price,description,types,images}:any) => (
+                            <Carditems key={id} id={id} brand={brand} catagory={catagory} name={name} price={price} description={description} types={types} images={images}/>
+                            ))
+                        }
+                      </div>
+                    </div>
+                  </div>
+                  )
+              }
+          })()}
         <div className='flex justify-center my-8'>
           <Stack spacing={2}>
             <Pagination count={10} variant="outlined" shape="rounded" onChange={handleChange} />
@@ -37,4 +60,4 @@ const Gaming = () => {
   )
 }
 
-export default Gaming
+export default Laptops
