f34c83ea51670500707bf294686033337c9c38eb
Anteneh Solomon <antenehcs@gmail.com>
2023-01-29 10:29:15 +0300

    phone search

--- src/Components/Body/Phone/Phone.tsx
+++ src/Components/Body/Phone/Phone.tsx
@@ -29,6 +29,7 @@ const Phone = () => {
   const [page, SetPage] = useState(1);
   const [itemsthere, setitemsthere] = useState(false);
   const [post,setpost] = useState<any[]>([]);
+  const [search, setSearch] = useState('');
 
   const handleChange = (e:any, p:any) => {
     console.log(p);
@@ -51,6 +52,21 @@ const Phone = () => {
           <div className='shadow border-b-2 border-gray-400 mx-4 md:mx-16 mt-8'>
             <h1>Shop Brand New Phones</h1>
           </div>
+          <div className="relative flex items-center h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden mx-4 md:mx-16 mt-2">
+              <div className="grid place-items-center h-full w-12 text-gray-300">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                  </svg>
+              </div>
+              <input
+              onChange={(e) => {
+                    setSearch(e.target.value.toLowerCase());
+                  }}
+              className="peer h-full outline-none text-sm text-gray-700 pr-2"
+              type="text"
+              id="search"
+              placeholder="Search phones.." /> 
+          </div>
           {(()=>{
               if(itemsthere){
                   return(
@@ -70,7 +86,9 @@ const Phone = () => {
                     <div className='flex flex-row '>
                       <div className='md:mx-16 flex flex-wrap justify-center align-center md:flex-wrap md:pr-8 mt-2'>
                         {
-                          [...post].slice((page-1)*12,12*page).map(({id,brand,name,catagory,price,description,types,images}:phone) => (
+                          [...post].filter((item:any) => {
+                            return search.toLowerCase() === '' ? item : item.name.toLowerCase().includes(search) || item.brand.toLowerCase().includes(search) || item.price.toLowerCase().includes(search)
+                          }).slice((page-1)*12,12*page).map(({id,brand,name,catagory,price,description,types,images}:phone) => (
                             <Carditems key={id} id={id} brand={brand} catagory={catagory} name={name} price={price} description={description} types={types} images={images}/>
                             ))
                         }
